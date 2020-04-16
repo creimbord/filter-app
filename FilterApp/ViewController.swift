af7e4e6c5d1f2ff361a294e1dcf78193c2dfadae
Rodion Artyukhin <artyukhin.rodion@gmail.com>
2020-04-16 20:05:41 +0300

    Initial Commit

---
+++ FilterApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FilterApp
+//
+//  Created by Родион on 16.04.2020.
+//  Copyright © 2020 Родион. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
